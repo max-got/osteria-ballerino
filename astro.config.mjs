0ef5235ac3b7f804790a1584c0da64144e8ce5ee
Max <max.gottschalk@mailbox.org>
2023-05-03 12:07:25 +0200

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
+import cloudflare from '@astrojs/cloudflare';
 
 
 // https://astro.build/config
 export default defineConfig({
       output: 'server',
+      adapter: cloudflare(),
       integrations: [
             tailwind({ config: { applyBaseStyles: false } }),
             svelte()
